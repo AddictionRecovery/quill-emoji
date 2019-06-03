fa2afc2939e78b3e1edfb32d15118e64bd9015e4
Stanislav Agapov <agpv.st@gmail.com>
2019-06-03 19:49:10 +0300

    Updated AR emoji module and styles

--- src/module-ar-emoji.js
+++ src/module-ar-emoji.js
@@ -1,8 +1,14 @@
 import Quill from 'quill'
-import Fuse from 'fuse.js'
 import emojiList from './emoji-list.js'
 
 const Module = Quill.import('core/module');
+
+/**
+ * Sort emoji properly.
+ */
+emojiList.sort(function (a, b) {
+  return a.emoji_order - b.emoji_order;
+});
 
 /**
  * Provides toolbar button and emoji dropdown with ability to filter emoji.
@@ -12,43 +18,27 @@ class ArEmoji extends Module {
   constructor(quill, options) {
     super(quill, options);
 
-    // Bind methods.
-    this.togglePalette = this.togglePalette.bind(this);
-    this.closePalette  = this.closePalette.bind(this);
-    this.openPalette   = this.openPalette.bind(this);
-    this.onFilterEmoji = this.onFilterEmoji.bind(this);
-
     /**
-     * Currently active emoji tab.
-     *
-     * @type {string}
-     * @private
-     */
-    this._activeTab = 'p';
-
-    /**
-     * Reference to Quill instance.
-     *
-     * @type {Quill}
-     * @private
-     */
-    this._quill = quill;
-
-    /**
-     * Reference to the Toolbar module instance.
+     * Toolbar module instance.
      *
      * @type {Module}
-     * @private
      */
-    this._toolbar = quill.getModule('toolbar');
+    const toolbar = quill.getModule('toolbar');
+
+    /**
+     * Emoji palette.
+     *
+     * @type {EmojiPalette}
+     */
+    const palette = new EmojiPalette(quill);
 
     // Initialize emoji toolbar button.
-    if (typeof this._toolbar !== 'undefined') {
+    if (typeof toolbar !== 'undefined') {
       // Add click handler to the emoji button.
-      this._toolbar.addHandler('emoji', this.togglePalette);
+      toolbar.addHandler('emoji', palette.toggle);
 
       // Set emoji button picture.
-      const btn = this._toolbar.container.querySelector('.ql-emoji');
+      const btn = toolbar.container.querySelector('.ql-emoji');
       if (btn) {
         btn.innerHTML = options.buttonIcon;
       }
@@ -56,120 +46,10 @@ class ArEmoji extends Module {
       // Close emoji palette when user starts typing.
       quill.on('text-change', (delta, oldDelta, source) => {
         if (source === 'user') {
-          this.closePalette();
+          palette.close();
         }
       });
     }
-  }
-
-  /**
-   * Shows or hides emoji palette.
-   */
-  togglePalette() {
-    if (!this.closePalette()) {
-      this.openPalette();
-    }
-  }
-
-  /**
-   * Closes emoji palette if it's open.
-   *
-   * @return {boolean}
-   *   TRUE if open palette was closed, FALSE otherwise.
-   */
-  closePalette() {
-    const quill = this._quill,
-          palette = quill.container.getElementsByClassName("emoji-palette");
-
-    if (palette) {
-      palette.remove();
-      return true;
-    }
-
-    return false;
-  }
-
-  /**
-   * Opens emoji palette.
-   */
-  openPalette() {
-    const quill = this._quill,
-          palette = document.createElement('div'),
-          range = quill.getSelection(),
-          bounds = quill.getBounds(range.index);
-
-    // Add emoji palette to the Quill container.
-    palette.classList.add('emoji-palette');
-    quill.container.appendChild(palette);
-
-    // Adjust emoji palette position.
-    const paletteMaxPos = bounds.left + 250; //palette max width is 250
-    palette.style.top = 10 + bounds.top + bounds.height + "px";
-    if (paletteMaxPos > quill.container.offsetWidth) {
-      palette.style.left = (bounds.left - 250)+ "px";
-    }
-    else{
-      palette.style.left = bounds.left + "px";
-    }
-
-    // Emoji category tabs.
-    const tabs = document.createElement('div');
-    tabs.classList.add('emoji-tabs');
-    palette.appendChild(tabs);
-
-    // Emoji search input.
-    const search = document.createElement('div');
-    search.classList.add('emoji-search');
-    palette.appendChild(search);
-    const searchInput = document.createElement('input');
-    searchInput.setAttribute('type', 'text');
-    searchInput.classList.add('emoji-search-input');
-    searchInput.addEventListener('input', this.onFilterEmoji);
-    search.appendChild(searchInput);
-
-    // Emoji list.
-    let list = document.createElement('div');
-    list.classList.add('emoji-list');
-    palette.appendChild(list);
-
-    const tabElementHolder = document.createElement('ul');
-    tabs.appendChild(tabElementHolder);
-
-    if (document.getElementById('emoji-close-div') === null) {
-      let closeDiv = document.createElement('div');
-      closeDiv.id = 'emoji-close-div';
-      closeDiv.addEventListener("click", this.closePalette, false);
-      document.getElementsByTagName('body')[0].appendChild(closeDiv);
-    }
-    else{
-      document.getElementById('emoji-close-div').style.display = "block";
-    }
-
-
-    emojiType.map(function(emojiType) {
-      //add tab bar
-      let tabElement = document.createElement('li');
-      tabElement.classList.add('emoji-tab');
-      tabElement.classList.add('filter-'+emojiType.name);
-      tabElement.innerHTML = emojiType.content;
-      tabElement.dataset.filter = emojiType.type;
-      tabElementHolder.appendChild(tabElement);
-
-      let emojiFilter = document.querySelector('.filter-'+emojiType.name);
-      emojiFilter.addEventListener('click',function(){
-        let tab = document.querySelector('.active');
-        if (tab) {
-          tab.classList.remove('active');
-        }
-        emojiFilter.classList.toggle('active');
-        fn_updateEmojiContainer(emojiFilter,list,quill);
-      })
-    });
-    fn_emojiPanelInit(list,quill);
-  }
-
-  onFilterEmoji(e) {
-    console.error(e)
   }
 
 }
@@ -188,7 +68,7 @@ ArEmoji.DEFAULTS = {
  *
  * @type {*[]}
  */
-const emojiType = [
+const emojiTypes = [
   { type: 'p', name: 'people' },
   { type: 'n', name: 'nature' },
   { type: 'd', name: 'food' },
@@ -199,73 +79,177 @@ const emojiType = [
   { type: 'f', name: 'flags' }
 ];
 
+/**
+ * Represents emoji palette element.
+ */
+class EmojiPalette {
 
-function fn_close(){
-  const ele_emoji_plate = document.getElementById('emoji-palette');
-  document.getElementById('emoji-close-div').style.display = "none";
-  if (ele_emoji_plate) {ele_emoji_plate.remove()}
-}
+  constructor (quill) {
 
-function fn_updateRange(quill){
-  return quill.getSelection();
-}
+    /**
+     * Quill instance.
+     *
+     * @type {Quill}
+     * @private
+     */
+    this._quill = quill;
 
-function fn_emojiPanelInit(panel,quill){
-  fn_emojiElementsToPanel('p', panel, quill);
-  document.querySelector('.filter-people').classList.add('active');
-}
+    /**
+     * Emoji palette element.
+     *
+     * @type {Element}
+     * @private
+     */
+    this._palette = null;
 
-function fn_emojiElementsToPanel(type,panel,quill){
-  let fuseOptions = {
-    shouldSort: true,
-    matchAllTokens: true,
-    threshold: 0.3,
-    location: 0,
-    distance: 100,
-    maxPatternLength: 32,
-    minMatchCharLength: 3,
-    keys: [
-      "category"
-    ]
-  };
-  let fuse = new Fuse(emojiList, fuseOptions);
-  let result = fuse.search(type);
-  result.sort(function (a, b) {
-    return a.emoji_order - b.emoji_order;
-  });
-
-  quill.focus();
-  let range = fn_updateRange(quill);
-
-  result.map(function(emoji) {
-    let span = document.createElement('span');
-    let t = document.createTextNode(emoji.shortname);
-    span.appendChild(t);
-    span.classList.add('bem');
-    span.classList.add('bem-' + emoji.name);
-    span.classList.add('ap');
-    span.classList.add('ap-' + emoji.name);
-    let output = '' + emoji.code_decimal + '';
-    span.innerHTML = output + ' ';
-    panel.appendChild(span);
-
-    let customButton = document.querySelector('.bem-' + emoji.name);
-    if (customButton) {
-      customButton.addEventListener('click', function() {
-        quill.insertEmbed(range.index, 'emoji', emoji, Quill.sources.USER);
-        setTimeout(() => quill.setSelection(range.index + 1), 0);
-        fn_close();
-      });
-    }
-  });
-}
-
-function fn_updateEmojiContainer(emojiFilter,panel,quill){
-  while (panel.firstChild) {
-    panel.removeChild(panel.firstChild);
+    // Bind methods.
+    this.toggle  = this.toggle.bind(this);
+    this.open    = this.open.bind(this);
+    this.close   = this.close.bind(this);
+    this.filter  = this.filter.bind(this);
+    this.render  = this.render.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
-  const type = emojiFilter.dataset.filter;
-  fn_emojiElementsToPanel(type,panel,quill);
+
+  /**
+   * Shows or hides emoji palette.
+   */
+  toggle() {
+    if (this._palette) {
+      this.close();
+    }
+    else {
+      this.open();
+    }
+  }
+
+  /**
+   * Opens emoji palette if it's close.
+   */
+  open() {
+    if (!this._palette) {
+      this._quill.focus();
+
+      const palette = document.createElement('div');
+      this._palette = palette;
+
+      // Create emoji palette and add it to the Quill container.
+      palette.classList.add('emoji-palette');
+      this._quill.container.appendChild(palette);
+
+      // Create emoji category tabs wrapper and add it to the emoji palette.
+      const tabs = document.createElement('ul');
+      tabs.classList.add('emoji-tabs');
+      palette.appendChild(tabs);
+
+      // Create emoji category tabs and add them to the tabs wrapper.
+      emojiTypes.forEach((emojiType) => {
+        const tab = document.createElement('li');
+        tab.classList.add('emoji-tab', 'emoji-tab-' + emojiType.name);
+        tab.dataset.category = emojiType.type;
+        tab.addEventListener('click', () => {
+          this.filter({ category: emojiType.type });
+        });
+        tabs.appendChild(tab);
+      });
+
+      // Created emoji search and add it to the palette.
+      const search = document.createElement('input');
+      search.setAttribute('type', 'text');
+      search.classList.add('emoji-search');
+      search.addEventListener('input', this.filter);
+      palette.appendChild(search);
+
+      // Create emoji wrapper and add it to the palette.
+      const emojiWrapper = document.createElement('div');
+      emojiWrapper.classList.add('emoji-list');
+      palette.appendChild(emojiWrapper);
+
+      // Show first tab by default.
+      this.filter({ category: 'p' });
+    }
+  }
+
+  /**
+   * Closes emoji palette if it's open.
+   */
+  close() {
+    if (this._palette) {
+      this._palette.remove();
+      this._palette = null;
+    }
+  }
+
+  /**
+   * Filters emoji list.
+   *
+   * @param {{}} e
+   *   Input/filter event.
+   */
+  filter(e) {
+    const tabs = this._palette.querySelector('.emoji-tabs'),
+          activeTab = tabs.querySelector('.active');
+
+    // Remove active tab, if any.
+    if (activeTab) {
+      activeTab.classList.remove('active');
+    }
+
+    // Filter emoji by category.
+    if (typeof e.category === 'string') {
+      // Set new active tab.
+      tabs.querySelector('[data-category="' + e.category + '"]').classList.add('active');
+
+      // Do filter emoji by category.
+      this.render(emojiList.filter((emoji) => emoji.category === e.category));
+    }
+    // Filter emoji by search query.
+    else {
+      this.render(emojiList.filter((emoji) => emoji.name.includes(e.target.value)));
+    }
+  }
+
+  /**
+   * Adds set of emoji to the emoji palette.
+   *
+   * @param {[]} emojiList
+   *   Array of emoji to add.
+   */
+  render(emojiList) {
+    const wrapper = this._palette.querySelector('.emoji-list');
+
+    // Remove old emoji from the palette.
+    while (wrapper.firstChild) {
+      wrapper.removeChild(wrapper.firstChild);
+    }
+
+    // Add new emoji to the palette.
+    emojiList.forEach((data) => {
+      const emoji = document.createElement('span');
+      emoji.classList.add('bem', 'bem-' + data.name, 'ap', 'ap-' + data.name);
+      emoji.innerHTML = '' + data.code_decimal + '';
+      emoji.addEventListener('click', this.onClick);
+      wrapper.appendChild(emoji);
+    });
+  }
+
+  /**
+   * Handles emoji clicks.
+   */
+  onClick() {
+    const quill = this._quill,
+          range = quill.getSelection();
+
+    // Insert emoji.
+    quill.insertEmbed(range.index, 'emoji', data, Quill.sources.USER);
+
+    // Move cursor after insert.
+    setTimeout(() => quill.setSelection(range.index + 1), 0);
+
+    // Close emoji palette.
+    this.close();
+  }
+
 }
 
 export default ArEmoji;
